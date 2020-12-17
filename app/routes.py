b6fb8291bf73c0fcb5baeb436b0a6fe32791fe24
Sasafrass <albertharkema@gmail.com>
2020-12-17 08:04:11 +0100

    work on template

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,32 @@
 from flask import render_template
 from app import app
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'Albert'}
-    return render_template('index.html', title='Home', user=user, cheese="Cheeseball")
+
+    # Generate a fake greeting
+    rand = random.uniform(0, 1)
+    if rand > 0.5:
+        user = {'username':'Albert'}
+    else:
+        user = {'username':'Fredje'}
+
+    # Generate fake posts
+    posts = [
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body': 'I like cheese'
+        }
+    ]
+
+    return render_template('index.html', user=user, cheese="Cheeseball", posts=posts)
+
 
 @app.route('/stront')
 def stront():
