57a54d859989086723927bc28d556d93f90ffdc0
Sasafrass <albertharkema@gmail.com>
2020-12-19 22:57:22 +0100

    up until registration

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,21 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
-import random
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 
-    # Generate a fake greeting
-    rand = random.uniform(0, 1)
-    if rand > 0.5:
-        user = {'username':'Albert'}
-    else:
-        user = {'username':'Fredje'}
+    # # Generate a fake greeting
+    # rand = random.uniform(0, 1)
+    # if rand > 0.5:
+    #     user = {'username':'Albert'}
+    # else:
+    #     user = {'username':'Fredje'}
 
     # Generate fake posts
     posts = [
@@ -26,17 +29,31 @@ def index():
         }
     ]
 
-    return render_template('index.html', user=user, cheese="Cheeseball", posts=posts)
+    return render_template('index.html', cheese="Cheeseball", posts=posts)
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me={}".format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next') # Did we get redirected to login from restricted page?
+        if not next_page or url_parse(next_page).netloc != '': # Ensures redirect within same site
+            next_page = url_for('index')
+        return redirect(next_page) # Redirects either to index or original 'restricted' page
     return render_template("login.html", title="Sign In", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/stront')
 def stront():
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     print(SECRET_KEY)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
