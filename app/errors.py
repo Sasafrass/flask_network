99bc836d51f1064898d74f0f67383cbcaae88feb
Sasafrass <albertharkema@gmail.com>
2020-12-21 22:32:57 +0100

    pagination

---
+++ app/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app, db
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404 # Add error code number instead of default 200.
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback() # Ensure we don't effectuate faulty database changes.
+    return render_template('500.html'), 500
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,34 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
+from datetime import datetime
 from werkzeug.urls import url_parse
 
-@app.route('/')
-@app.route('/index')
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
 
-    # # Generate a fake greeting
-    # rand = random.uniform(0, 1)
-    # if rand > 0.5:
-    #     user = {'username':'Albert'}
-    # else:
-    #     user = {'username':'Fredje'}
+    # Generate interesting posts
+    posts = current_user.followed_posts().all()
 
-    # Generate fake posts
-    posts = [
-        {
-            'author':{'username':'John'},
-            'body':'Beautiful day in Portland!'
-        },
-        {
-            'author':{'username':'Susan'},
-            'body': 'I like cheese'
-        }
-    ]
-
-    return render_template('index.html', cheese="Cheeseball", posts=posts)
+    return render_template('index.html', cheese="Cheeseball", form=form, posts=posts)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -80,8 +79,65 @@ def user(username):
         {'author':user, 'body':'Test post #1'},
         {'author':user, 'body':'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
+
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved!")
+        return redirect(url_for('edit_profile'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>', methods=['GET','POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash(f'User {username} not found')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('users', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f"You are following {username}!")
+        return redirect(url_for('user', username=username)) # Form is rendered by user route!
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash(f'User {username} not found')
+            redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f'You are not following {username}')
+        return redirect(url_for('user', username=username)) # Form is rendered by user route!
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/stront')
 def stront():
